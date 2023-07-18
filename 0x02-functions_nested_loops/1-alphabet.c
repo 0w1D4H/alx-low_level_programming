0320d5245faf626a578087803a47383a76fa9c00
DrYehu <doctoryehumusic@gmail.com>
2023-07-18 04:05:30 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,11 @@
-#include "main.h"
-
-/**
- *print_alphabet - prints the alphabet, in lowercase, followed by a new line.
- *
- * Return: Always 0 (success)
- */
 void print_alphabet(void)
 {
-	int ch;
+  char c;
 
+  for (c = 'a'; c <= 'z'; c++)
+  {
+    _putchar(c);
+  }
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
-}
+  _putchar('\n');
+}}
