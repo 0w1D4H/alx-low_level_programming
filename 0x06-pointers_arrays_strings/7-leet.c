28482ec61f4fbd80eddd43e971f7dd8eba5fff4f
DrYehu <doctoryehumusic@gmail.com>
2023-07-26 10:58:42 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *cap_string - ...
- *@s: ...
- *Return: char value
+ *leet-Encodes a string into 1337
+ *@s:The string to encode
+ *Return:The encoded string
 */
 
-char *cap_string(char *s)
+char *leet(char *s)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
 
 	while (s[a])
 	{
-	i = 0;
-	while (i < cspc)
+	b = 0;
+	while (b < l)
 	{
-	if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-	s[a] -= 32;
-	i++;
+	if (s[a] == r[b] || s[a] - 32 == r[b])
+	{
+	s[a] = n[b];
+	}
+	b++;
 	}
 	a++;
 	}
